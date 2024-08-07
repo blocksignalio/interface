7209a1ef795820c92e2bea62617573ecb8149f1e
Yordan Miladinov <yordanm@proton.me>
2024-08-07 14:07:53 +0300

    lazy deployment script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+git branch -D deployment
+git checkout -b deployment
 rm -rf dist docs
 yarn build
 mv dist docs
 cp CNAME docs/
+git add --force -- docs
+git commit -m deploy
+git push origin deployment --force
+git checkout master
+rm -rf docs
